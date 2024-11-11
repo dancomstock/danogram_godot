569adebe0febe052ed7ebd253e01d758b2967c95
dancomstock <dan.j.comstock@gmail.com>
2024-11-11 10:35:02 -0600

    add splash screen

--- python/image.py
+++ python/image.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import numpy as np
-im = Image.open("apple5.png")
-print(im)
+imo = Image.open("apple5.png")
+print(imo)
 # im = im.load()
 # a = np.asarray(im)
 # print(a)
@@ -15,6 +15,8 @@ def rgb_to_hex(red, green, blue):
 	"""Return color as #rrggbb for the given color values."""
 	return '#%02x%02x%02x' % (red, green, blue)
 
+im = imo.quantize(4).convert('RGB')
+print(im)
 # print(im.getpalette())
 solution = []
 palette = ['#ffffff']
@@ -24,7 +26,9 @@ for i in range(y):
 	solution.append([])
 	for j in range(x):
 		print(f'{i},{j}')
-		r, g, b, _ = im.getpixel((j,i))
+		print(im.getpixel((j,i)))
+		# print(im[j,i])
+		r, g, b, = im.getpixel((j,i))
 		# print(pixel)
 		color = rgb_to_hex(r,g,b)
 		if color not in palette:
@@ -34,5 +38,6 @@ for i in range(y):
 		# l[i].append(rgb_to_hex(*im.getpixel((j,i))))
 
 # print(l)
+im.save('apple_new3.png', 'png')
 print(f'solution:{solution}')
 print(f'palette:{palette}')
---
+++ start_button.gd
@@ -0,0 +1,17 @@
+extends Button
+
+
+var main_scene = load("res://main.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.pressed.connect(self._button_pressed)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _button_pressed():
+	get_tree().change_scene_to_packed(main_scene)
